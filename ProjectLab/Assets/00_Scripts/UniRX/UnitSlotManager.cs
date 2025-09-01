8d1689a091c004ad39dadd14a4fd887cf9863205
Hambugi <mvat1225@naver.com>
2025-09-01 23:24:29 +0900

    UniRX Sample Code

--- ProjectLab/Assets/00_Scripts/UniRX/UnitSlotManager.cs
+++ ProjectLab/Assets/00_Scripts/UniRX/UnitSlotManager.cs
@@ -1,7 +1,46 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using UnityEngine.InputSystem;
 
 public class UnitSlotManager : MonoBehaviour
 {
+    private List<Unit> _units = new();
+
+    private readonly ReactiveProperty<int> _changedUnitCount = new();
+    public IReadOnlyReactiveProperty<int> ChangedUnitCount => _changedUnitCount;
+
+
+    private void Awake()
+    {
+        SetUpAction();
+    }
+
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            SpawnUnit(new());
+        }
+    }
+
+    public void SpawnUnit(Unit unit)
+    {
+        _units.Add(unit);
+        _changedUnitCount.Value = _units.Count;
+    }
+
+    private void SetUpAction()
+    {
+        _changedUnitCount.Subscribe(OnChangedUnitCount).AddTo(this);
+    }
+    
+    private void OnChangedUnitCount(int count)
+    {
+        Debug.Log($"Changed UnitCount => {count}");
+    }
     
     
 }
